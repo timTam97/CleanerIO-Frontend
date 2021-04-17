74a3ef5971a2d99e48ac43e7e8f478ef173de62c
eddie <etan72@student.monash.edu>
2021-04-17 12:58:37 +1000

    added update button YE

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function TabTwoScreen() {
@@ -25,8 +25,14 @@ export default function TabTwoScreen() {
     return <Text>No access to camera</Text>;
   }
 
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => alert('Logs have been updated')} style={styles.button}>
+        <Text style={styles.buttonText}>Update Logs</Text>
+      </TouchableOpacity>
+      
+      
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         //style={StyleSheet.absoluteFillObject}
@@ -36,6 +42,8 @@ export default function TabTwoScreen() {
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -50,6 +58,36 @@ const styles = StyleSheet.create({
   left: 35,
   bottom: 220,
   right: 35
-
-  }
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    position: 'absolute',
+    top: 500,
+    right: 100,
+    left: 100,
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign:'center',
+  },
 });
